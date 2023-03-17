7cca1ee2a1d4974e0d519e681fdf878df7b74c3b
rahulpareek3366 <103638697+rahulpareek3366@users.noreply.github.com>
2023-03-17 15:03:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "rahul-first" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "RahulJenkins-TFTest"
   }
 }
